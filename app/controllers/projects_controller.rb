250a90d95d0a2124929605fd3dd598c8c347f94b
Mohammed Sadiq <sadiqmmm@gmail.com>
2018-05-08 07:38:42 +0530

    redirect user if they try to find non existing projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -47,7 +47,10 @@ class ProjectsController < ApplicationController
       params.require(:project).permit(:name, :description)
     end
 
-    def set_project
-      @project = Project.find(params[:id])
+    def set_project     
+        @project = Project.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        flash[:alert] = 'The project you were looking for could not be found.'
+      redirect_to projects_path      
     end
 end
