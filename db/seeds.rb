8e4c3bdbaa9b8b2fa0736d4fba8c77c7d88f260e
Mohammed Sadiq <sadiqmmm@gmail.com>
2018-05-12 00:51:12 +0530

    Add admin flag to User model and add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Create admin if not exist
+unless User.exists?(email: "admin@ticketee.com")
+  puts "Admin Created"
+  User.create!(email: "admin@ticketee.com", password: "password", admin: true)
+end
+
+unless User.exists?(email: "viewer@ticketee.com")
+  puts "Viewer Created"
+  User.create!(email: "viewer@ticketee.com", password: "password")
+end
+
+["Sublime Text 3", "Internet Explorer"].each do |name|
+  unless Project.exists?(name: name)
+    puts "Project Created"
+    Project.create!(name: name, description: "A sample project about #{name}", author_id: 1)
+  end
+end
